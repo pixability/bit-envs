076866cf0a023f3c556c0ea72d76e480f12580e2
Aram Drevekenin <grimsniffer@gmail.com>
2018-10-09 16:40:19 +0200

    test(mocha): test filesRequire functionality

--- test/mocha/fixture/test.spec.js
+++ test/mocha/fixture/test.spec.js
@@ -5,4 +5,7 @@ describe('basic', function (){
     it('should add properly', function() {
         expect(run()).to.equal(0)
     })
+    it('should preload a file specified with `filesRequire` in the config', function() {
+        expect(global.mochaSetupTestRun).to.equal(true)
+    })
 })
